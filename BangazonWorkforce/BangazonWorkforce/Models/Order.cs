4fe9a3e7a639e67c45e9dd31f0e873214b2da033
Your Name <you@example.com>
2020-09-01 14:33:29 -0400

    created boiler plate files

---
+++ BangazonWorkforce/BangazonWorkforce/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Order
+    {
+        [Required]
+        public int Id { get; set; }
+        public int? PaymentTypeId { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        public List<Product> productsInOrder { get; set; } = new List<Product>();
+        public Customer customerForOrder { get; set; } = new Customer();
+
+    }
+}
---
+++ BangazonWorkforce/BangazonWorkforce/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int ProductTypeId { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        public Decimal Price { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
