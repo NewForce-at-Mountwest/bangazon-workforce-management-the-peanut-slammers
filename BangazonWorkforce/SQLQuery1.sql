18452d42f8cc1cd927420c4052a565c2a8761008
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-09-02 15:30:49 -0400

    successfully picking out only current computer via sql query select statement

--- BangazonWorkforce/SQLQuery1.sql
+++ BangazonWorkforce/SQLQuery1.sql
@@ -1,11 +1,11 @@
-﻿-- SELECT e.Id, e.FirstName, e.LastName, d.Name AS 'Department', c.Make AS 'Computer', t.Name AS 'Trainings'
--- FROM Employee e JOIN Department d 
--- ON e.DepartmentId = d.Id 
--- JOIN ComputerEmployee x ON e.Id = x.EmployeeId
--- JOIN Computer c ON x.ComputerId = c.Id
--- JOIN EmployeeTraining y ON e.Id = y.EmployeeId
- ---JOIN TrainingProgram t ON y.TrainingProgramId = t.Id
--- WHERE e.Id = 1;
+﻿ SELECT e.Id, e.FirstName, e.LastName, d.Name AS 'Department', c.Make AS 'Computer', t.Name AS 'Trainings'
+ FROM Employee e JOIN Department d 
+ ON e.DepartmentId = d.Id 
+ JOIN ComputerEmployee x ON e.Id = x.EmployeeId
+ JOIN Computer c ON x.ComputerId = c.Id
+ JOIN EmployeeTraining y ON e.Id = y.EmployeeId
+ JOIN TrainingProgram t ON y.TrainingProgramId = t.Id
+ WHERE e.Id = 1;
 
 --INSERT INTO EmployeeTraining (EmployeeId, TrainingProgramId) VALUES (1, 2);
 
