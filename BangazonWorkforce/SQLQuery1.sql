2d17d8cbdc169bbd932d1dd290505df5674c5940
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-09-03 11:03:51 -0400

    save before mergining with new master

--- BangazonWorkforce/SQLQuery1.sql
+++ BangazonWorkforce/SQLQuery1.sql
@@ -5,10 +5,12 @@
  JOIN Computer c ON x.ComputerId = c.Id
  JOIN EmployeeTraining y ON e.Id = y.EmployeeId
  JOIN TrainingProgram t ON y.TrainingProgramId = t.Id
- WHERE e.Id = 1 AND x.UnassignDate is null;
+ WHERE e.Id = 4 AND x.UnassignDate is null;
 
 --INSERT INTO EmployeeTraining (EmployeeId, TrainingProgramId) VALUES (1, 2);
 --INSERT INTO ComputerEmployee (EmployeeId, ComputerId, AssignDate, UnassignDate) VALUES (1, 3, '2010-09-01', '2015-09-01');
 --SELECT * FROM ComputerEmployee;
+
+
 
  
